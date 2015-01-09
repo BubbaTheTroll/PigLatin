e9ce86148c8d0471d2b6f960114214fdcf07068f
bubbathetroll <cai.alvan8@gmail.com>
2015-01-09 10:02:23 -0800

    Added first name to index.html

--- build-tmp/source/PigLatin.java
+++ build-tmp/source/PigLatin.java
@@ -1,4 +1,22 @@
-import java.util.*;
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class PigLatin extends PApplet {
+
+
 
 public void setup() {
 	
@@ -39,28 +57,35 @@ public String pigLatin(String sWord)
 {
 	if(findFirstVowel(sWord) == -1)
 	{
-
 		return sWord + "ay";
-
 	}
+	
+
 	else if (findFirstVowel(sWord) == 0) 
 	{
-
-		return sWord + "way";
-
+		return sWord + "way";	
 	}
-	else if ((sWord.substring(0,2)).equals("qu"))
-	{
+	else if ((sWord.substring(0,2)).equals("qu")){
 
 		return sWord.substring(2) + "quay"; 
-
 	}
+	
 	else if (findFirstVowel(sWord) > 0){
 
 		return (sWord.substring(findFirstVowel(sWord)) + sWord.substring(0, findFirstVowel(sWord)) + "ay");
 	}
+	
 	else
 	{
 		return "ERROR!";
 	}
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "PigLatin" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
